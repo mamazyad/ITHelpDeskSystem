6bdd454532fd942be73ef3871994158070de9359
mamazyad <mamazyad@dah.edu.sa>
2018-05-05 13:21:09 +0300

    5/5/2018

--- ITHelpDeskSystem/Migrations/Configuration.cs
+++ ITHelpDeskSystem/Migrations/Configuration.cs
@@ -68,11 +68,18 @@ namespace ITHelpDeskSystem.Migrations
                 new ITStaff { UserName = "itstaff", Email ="ITstaff1@gmail.com", FirstName ="Jack", LastName ="White", JobTitle ="User support ", Department="IT Department", ExtensionNumber = "544", Mobile = "0553334445", OfficeNumber = "B17", Speciality = " ", StartingDate = null, Position =" ", IsManager = false,
                     },
 
-                 new ITStaff { UserName = "itstaff1", Email ="ITstaff1@gmail.com", FirstName ="Jane", LastName ="Black", JobTitle ="Network admin", Department="IT Department", ExtensionNumber = "577", Mobile = "0553334447", OfficeNumber = "A15", Speciality = " ", StartingDate = null, Position =" ", IsManager = false,
+                 new ITStaff { UserName = "itstaff1", Email ="ITstaff1@gmail.com", FirstName ="Jane", LastName ="Black", JobTitle ="Network admin", Department="IT Department", ExtensionNumber = "577", Mobile = "0553334447", OfficeNumber = "C04", Speciality = " ", StartingDate = null, Position =" ", IsManager = false,
                     },
 
-                 new ITStaff { UserName = "itstaff2", Email ="ITstaff2@gmail.com", FirstName ="Mark", LastName ="Brown", JobTitle ="SAP specialist", Department="IT Department", ExtensionNumber = "322", Mobile = "0553334448", OfficeNumber = "C05", Speciality = " ", StartingDate = null, Position =" ", IsManager = false,
+                 new ITStaff { UserName = "itstaff2", Email ="ITstaff2@gmail.com", FirstName ="Majid", LastName ="Salem", JobTitle ="SAP specialist", Department="IT Department", ExtensionNumber = "322", Mobile = "0553334448", OfficeNumber = "C05", Speciality = " ", StartingDate = null, Position =" ", IsManager = false,
                     },
+
+                  new ITStaff { UserName = "mbrown", Email ="ITstaff3@gmail.com", FirstName ="Mark", LastName ="Brown", JobTitle ="System engineer", Department="IT Department", ExtensionNumber = "322", Mobile = "0553334448", OfficeNumber = "C06", Speciality = " ", StartingDate = null, Position =" ", IsManager = false,
+                    },
+
+                   new ITStaff { UserName = "mbrown", Email ="ITstaff3@gmail.com", FirstName ="Ali", LastName ="Kareem", JobTitle ="Factory technician", Department="IT Department", ExtensionNumber = "322", Mobile = "0553334448", OfficeNumber = "C06", Speciality = " ", StartingDate = null, Position =" ", IsManager = false,
+                    },
+
 
                  new ITStaff { UserName = "itmanager", Email ="itmanager@gmail.com", FirstName ="Mike", LastName ="Smith", JobTitle ="IT manager", Department="IT Department", ExtensionNumber = "741", Mobile = "0553334448", OfficeNumber = "A13", Speciality = " ", StartingDate = null, Position =" ", IsManager = true,
                     },
@@ -121,10 +128,31 @@ namespace ITHelpDeskSystem.Migrations
             //Adding Staff 
             var Staffs = new List<Staff>
             {
-                new Staff { UserName = "staff", Email ="HelpDesk@gmail.com", FirstName ="Peter", LastName ="Swan", JobTitle ="Recruiter", Department="HR", ExtensionNumber = " ", Mobile = " ", OfficeNumber = " ", StaffLevel = " ", ManagerialPosition = ManagerialPosition.Regular,
+                new Staff { UserName = "staff", Email ="HelpDesk@gmail.com", FirstName ="Khalid", LastName ="Saad", JobTitle ="Recruiter", Department="HR", ExtensionNumber = " ", Mobile = " ", OfficeNumber = " ", StaffLevel = " ", ManagerialPosition = ManagerialPosition.Regular,
                   },
 
                 new Staff { UserName = "staff1", Email ="Staff1@gmail.com", FirstName ="Suzan", LastName ="Adam", JobTitle ="Brand manager", Department="Marketing", ExtensionNumber = " ", Mobile = " ", OfficeNumber = " ", StaffLevel = " ", ManagerialPosition = ManagerialPosition.High,
+                  },
+
+                new Staff { UserName = "staff2", Email ="Staff2@gmail.com", FirstName ="Ahmad", LastName ="Ali", JobTitle ="HR manager", Department="HR", ExtensionNumber = " ", Mobile = " ", OfficeNumber = " ", StaffLevel = " ", ManagerialPosition = ManagerialPosition.High,
+                  },
+
+                new Staff { UserName = "staff3", Email ="Staff3@gmail.com", FirstName ="Malek", LastName ="Ahmad", JobTitle ="Sales", Department="Marketing", ExtensionNumber = " ", Mobile = " ", OfficeNumber = " ", StaffLevel = " ", ManagerialPosition = ManagerialPosition.High,
+                  },
+
+                 new Staff { UserName = "staff4", Email ="Staff4@gmail.com", FirstName ="Sara", LastName ="Murad", JobTitle ="Sales", Department="Marketing", ExtensionNumber = " ", Mobile = " ", OfficeNumber = " ", StaffLevel = " ", ManagerialPosition = ManagerialPosition.High,
+                  },
+
+                 new Staff { UserName = "staff5", Email ="Staff5@gmail.com", FirstName ="Salam", LastName ="Abdulrahman", JobTitle ="Sales", Department="Marketing", ExtensionNumber = " ", Mobile = " ", OfficeNumber = " ", StaffLevel = " ", ManagerialPosition = ManagerialPosition.High,
+                  },
+
+                 new Staff { UserName = "staff6", Email ="Staff6@gmail.com", FirstName ="Masood", LastName ="Khan", JobTitle ="Sales", Department="Marketing", ExtensionNumber = " ", Mobile = " ", OfficeNumber = " ", StaffLevel = " ", ManagerialPosition = ManagerialPosition.High,
+                  },
+
+                 new Staff { UserName = "staff7", Email ="Staff7@gmail.com", FirstName ="Muhannad", LastName ="Droobi", JobTitle ="Sales", Department="Marketing", ExtensionNumber = " ", Mobile = " ", OfficeNumber = " ", StaffLevel = " ", ManagerialPosition = ManagerialPosition.High,
+                  },
+
+                 new Staff { UserName = "staff8", Email ="Staff8@gmail.com", FirstName ="Sana", LastName ="Akram", JobTitle ="Sales", Department="Marketing", ExtensionNumber = " ", Mobile = " ", OfficeNumber = " ", StaffLevel = " ", ManagerialPosition = ManagerialPosition.High,
                   },
             };
 
@@ -148,9 +176,14 @@ namespace ITHelpDeskSystem.Migrations
 
                  new Category { CategoryName = "Software", CategoryDescription = "No description" ,ITStaffId = ITstaffs.Single(d=>d.UserName=="itstaff1").Id },
 
-                 new Category { CategoryName = "SAP", CategoryDescription = "No description", ITStaffId = ITstaffs.Single(d=>d.UserName=="itstaff2").Id},
+                 new Category { CategoryName = "SAP", CategoryDescription = "No description", ITStaffId = ITstaffs.Single(d=>d.UserName=="itstaff1").Id},
 
-                 new Category { CategoryName = "WiFi", CategoryDescription = "No description", ITStaffId = 2}
+               new Category { CategoryName = "WiFi", CategoryDescription = "No description", ITStaffId = ITstaffs.Single(d=>d.UserName=="itstaff").Id,},
+
+                 new Category { CategoryName = "Printer", CategoryDescription = "No description", ITStaffId = ITstaffs.Single(d=>d.UserName=="itstaff1").Id,},
+
+                 new Category { CategoryName = "Email", CategoryDescription = "No description", ITStaffId = ITstaffs.Single(d=>d.UserName=="itstaff1").Id,},
+
 
             };
             categories.ForEach(s => context.Categories.AddOrUpdate(p => p.CategoryName, s));
@@ -171,9 +204,19 @@ namespace ITHelpDeskSystem.Migrations
 
                 new KnowledgeBase { CreatedBy= ITstaffs.Single(d=>d.UserName=="itstaff2").Id, ITStaffId = ITstaffs.Single(d=>d.UserName=="itstaff2").Id, Topic = "WiFi", CreationDate=DateTime.Now, IncidentTitle="Connect to WiFi", IncidentDescription ="Connecting mobile devices to WiFi", SolutionDescription="Go to your device settings, WiFi options then enter your username and password" },
 
-                 new KnowledgeBase { CreatedBy= ITstaffs.Single(d=>d.UserName=="itstaff").Id, ITStaffId = ITstaffs.Single(d=>d.UserName=="itstaff").Id, Topic = "Hardware", CreationDate=DateTime.Now, IncidentTitle="Rebooting error from disck", IncidentDescription ="Error disck at booting", SolutionDescription="If you can hear a repeated scraping noise, power off the computer as soon as possible, as there may be a physical problem with the hard disk and you may lose data." },
+                 new KnowledgeBase { CreatedBy= ITstaffs.Single(d=>d.UserName=="itstaff").Id, ITStaffId = ITstaffs.Single(d=>d.UserName=="itstaff").Id, Topic = "Booting", CreationDate=DateTime.Now, IncidentTitle="Booting error from disck", IncidentDescription ="Error disck at booting", SolutionDescription="If you can hear a repeated scraping noise, power off the computer as soon as possible, as there may be a physical problem with the hard disk and you may lose data." },
 
-                      new KnowledgeBase { CreatedBy= ITstaffs.Single(d=>d.UserName=="itstaff").Id, ITStaffId = ITstaffs.Single(d=>d.UserName=="itstaff").Id, Topic = "Hardware", CreationDate=DateTime.Now, IncidentTitle="Rebooting error from disck", IncidentDescription ="Error disck at booting", SolutionDescription="If you can hear a repeated scraping noise, power off the computer as soon as possible, as there may be a physical problem with the hard disk and you may lose data." },
+   
+                   new KnowledgeBase { CreatedBy= ITstaffs.Single(d=>d.UserName=="itstaff").Id, ITStaffId = ITstaffs.Single(d=>d.UserName=="itstaff1").Id, Topic = "Connectivity", CreationDate=DateTime.Now, IncidentTitle="USB not recognized", IncidentDescription ="USB not recognized when blugged in to computer", SolutionDescription="This method resolves issues where the currently loaded USB driver has become unstable or corrupt. Select Start, type Device Manager in the Search box Select Device Manager from the returned list. Select Disk Drives from the list of hardware Press and hold (or right-click) the USB external hard drive with the issue, and select Uninstall. After the hard drive is uninstalled, unplug the USB cable. Wait for 1 minute and then reconnect the USB cable. The driver should automatically load Check for the USB drive in Windows Explorer" },
+
+                    new KnowledgeBase { CreatedBy= ITstaffs.Single(d=>d.UserName=="itstaff").Id, ITStaffId = ITstaffs.Single(d=>d.UserName=="itstaff1").Id, Topic = "Printer", CreationDate=DateTime.Now, IncidentTitle="Printer jammed", IncidentDescription ="Printer jammed when printing more than 50 pages", SolutionDescription="Removing a rear access panel Locate the knob or access tab on the back of the printer by or on the panel itself. If it's a knob, move it to the Unlocked position. Remove the panel and carefully pull out the jammed paper. Locate and clear away any small bits of paper that remain. Replace and secure the rear panel. Remove the two-sided printing accessory to clear the paper jam Press both RELEASE buttons on either end of the module at the same time and remove it." },
+
+                     new KnowledgeBase { CreatedBy= ITstaffs.Single(d=>d.UserName=="itstaff").Id, ITStaffId = ITstaffs.Single(d=>d.UserName=="itstaff1").Id, Topic = "Email", CreationDate=DateTime.Now, IncidentTitle="Connot login to email", IncidentDescription ="Connot log in to email feom this computer", SolutionDescription="Clear the cache and remove cookies only from websites that cause problems. Clear the Cache: Edit > Preferences > Advanced > Network > Cached Web Content: Clear Now, Remove Cookies from sites causing problems: Edit > Preferences > Privacy >  Use custom settings for history  > Cookies:  Show Cookies." },
+
+                     new KnowledgeBase { CreatedBy= ITstaffs.Single(d=>d.UserName=="itstaff").Id, ITStaffId = ITstaffs.Single(d=>d.UserName=="itstaff1").Id, Topic = "Software", CreationDate=DateTime.Now, IncidentTitle="Deleting browse history", IncidentDescription ="Connot log in to email feom this computer", SolutionDescription="On your computer, open Chrome. At the top right, click More  . Click History   History. On the left, click Clear browsing data. A box will appear. From the drop-down menu, select how much history you want to delete. To clear everything, select the beginning of time. Check the boxes for the info you want Chrome to clear, including “browsing history." },
+
+
+
             };
             knowledgeBase.ForEach(s => context.KnowledgeBases.AddOrUpdate(p => p.Topic, s));
             context.SaveChanges();
